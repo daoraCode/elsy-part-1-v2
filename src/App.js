570dc10ab8e7c8891fee8cfe1586aa180970c22b
daoraCode <severin.mboukou@gmail.com>
2021-11-07 08:30:42 +0100

    Box component created and imported in App.js, material icons created with custom sets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React, { Component } from 'react';
+import React from "react";
+import './App.css';
+import './styles/global.css';
+import Box from "./components/Box";
 
-class App extends Component {
+const tempMin = -20;
+// const tempMax = 40;
+const heartMin = 80;
+// const heartMax = 180;
+const stepsMin = 0;
+// const stepsMax = 50000;
+
+
+class App extends React.Component {
   render() {
     return (
-      <div>
-
+      <div className="container-fluid">
+        <div className="row">
+          <p>Heart: {heartMin}</p>
+          <p>Temperature: {tempMin}</p>
+          <p>Steps: {stepsMin}</p>
+          {/* Water */}
+          <Box 
+            icon="local_drink" 
+            color="#3A85FF" 
+            value="1.5" 
+            unit="L" 
+          />
+          {/* Steps */}
+          <Box 
+            icon="directions_walk" 
+            color="black" 
+            value="3000" 
+            unit="steps" 
+          />
+          {/* Heart */}
+          <Box 
+            icon="favorite" 
+            color="red" 
+            value="120" 
+            unit="bpm" 
+          />
+          <Box 
+            icon="local_drink" 
+            color="#3A85FF" 
+            value="1.5" 
+            unit="L" 
+          />
+        </div>
       </div>
     );
   }
 }
-
 export default App;
